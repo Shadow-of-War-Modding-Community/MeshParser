367a7df47321c11dbdf51d9efdf0cde3d0fb5ea9
xNightswitch <93552947+xNightswitch@users.noreply.github.com>
2025-05-04 18:38:02 +0200

    Added main function used for distribution

--- MeshUnpacker/Source/main.cpp
+++ MeshUnpacker/Source/main.cpp
@@ -1,13 +1,18 @@
 #include "Parser/includes-types.h"
 
-int main() {
+#include <filesystem>
+
+int main(int argc, char* argv[]) {
 	using namespace PARSER;
+	if (argc < 2) return 0;
 
-	auto mesh = Parser::Import("./Tests/player_rangerswordt1.mesh");
-
-	Parser::Export(mesh, "./Tests/player_rangerswordt1.fbx", "fbx");
-
-	auto fbx = Parser::Import("./Tests/player_rangerswordt1.fbx", &mesh);
-
-	Parser::Export(fbx, "./Tests/player_rangerswordt1-fbx.mesh", "mesh");
+	if (std::string(argv[1]) == "unpack") {
+		auto mesh = Parser::Import(argv[2]);
+		Parser::Export(mesh, std::filesystem::path(std::string(argv[2])).parent_path().string() + "\\out.fbx", "fbx");
+	}
+	if (std::string(argv[1]) == "repack") {
+		auto ref = Parser::Import(argv[3]);
+		auto mesh = Parser::Import(argv[2], &ref);
+		Parser::Export(mesh, std::filesystem::path(std::string(argv[2])).parent_path().string() + "\\out.mesh", "mesh");
+	}
 }
